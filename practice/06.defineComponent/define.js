f36ecb9ddd7a0fccfb96b2ff35d7bda3717dcabf
anunwu <943486950@qq.com>
2020-02-04 18:04:46 +0800

    添加了组件定义关于子组件修改父组件以及手动设置父组件的内容

--- practice/06.defineComponent/define.js
+++ practice/06.defineComponent/define.js
@@ -4,6 +4,10 @@ const data = {
   text: 0
 }
 
+const parent = new Vue({
+  name: 'parent'
+})
+
 const component = {
   // props: ['active', 'propOne']
   // props: {
@@ -40,6 +44,13 @@ const component = {
       text: 0
     }
   },
+  mounted() {
+    // 查看父组件的名称, 即组件定义中的属性name
+    // this.$parent.name ==> undefined 由于name定义后不会出挂载到组件的实例中，而是挂载到组件的$options对象中
+    console.log('我的父组件是', this.$parent.$options.name)
+    // 通过this.$parent直接修改父组件中的data，但不建议这么做，会导致后期维护较为困难
+    this.$parent.text = '我是子组件'
+  },
   methods: {
     handleChange () {
       this.$emit('change')
@@ -51,8 +62,10 @@ const component = {
 // 命名规则：首字母大写，采用驼峰命名
 // 但在html中不区分大小写且可用-代替驼峰
 // Vue.component('CompOne', component)
-
 new Vue({
+  // 设置父组件
+  parent: parent,
+  name: 'Root',
   // 局部注册组件
   components: {
     CompOne: component
@@ -62,10 +75,12 @@ new Vue({
     <div>
       <comp-one :active="true" :prop-one="text" @change="handleChanger"></comp-one>
       <comp-one :activce="false" ref="comp2"></comp-one>
+      <span>{{ text }}</span>
     </div>
   `,
   mounted () {
-    console.log(this.$refs.comp2)
+    // console.log(this.$refs.comp2)
+    console.log(this.$parent.$options.name)
   },
   data: {
     text: 'text1'
@@ -75,5 +90,4 @@ new Vue({
       this.text += 1
     }
   }
-
 })
