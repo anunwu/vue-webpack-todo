71276770e59dacfa26154f3e4ffc1cdaff2e8de4
anunwu <943486950@qq.com>
2020-02-05 22:04:10 +0800

    添加了vue-router的基本搭建

--- client/index.js
+++ client/index.js
@@ -1,11 +1,15 @@
 import Vue from 'vue'
 import App from './app.vue'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
 
 import './assets/styles/global.styl'
+import createRouter from './config/router'
 
-const root = document.createElement('div')
-document.body.appendChild(root)
+const router = createRouter()
 
 new Vue({
+  router,
   render: (h) => h(App)
-}).$mount(root)
+}).$mount('#root')
